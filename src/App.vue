9c98fc72e88fe24db3a15b96630d48640e61d188
ShadowP1e <andrey.bakov.2006@gmail.com>
2020-12-10 20:01:36 +0300

    project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <TodoList v-bind:todos="todos" v-on:delete-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList'
 
 export default {
-  name: 'App',
+  name: 'app',
+  data(){
+    return {
+      todos: ['Зарядка', 'Завтрак', 'Занятия', "Обед"]
+    }
+  },
   components: {
-    HelloWorld
+    TodoList
+  },
+  methods: {
+    deleteTodo(index){
+      this.todos.splice(index, 1);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
+  background-color: lightblue;
+}
+.list{
+  height: 500px;
+  width: 350px;
+  align-items: center;
+}
+.title{
+  background-color: blueviolet;
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 1px;
+  border-radius: 15px;
+}
+.doings{
+  height: auto;
+  width: 320px;
+  background-color: white;
+  padding: 15px;
+  margin-top: 15px;
+  border-radius: 20px;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="list">
+    <div class="title">
+    <h3>Список дел на сегодня</h3>
+    </div>
+    <div class="doings">
+    <ul>
+        <li class='item' v-for="(todo, index) in todos" v-on:click="deleteTodo(index)" v-bind:key="index">{{ todo }}</li>
+    </ul>
+    </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['todos'],
+    methods: {
+        deleteTodo(index){
+            this.$emit('delete-todo', index);
+        }
+    }
+}
+</script>
